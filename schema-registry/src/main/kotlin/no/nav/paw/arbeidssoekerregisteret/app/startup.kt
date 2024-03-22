75660771203f2d895866fa8939b2e440ed8a2549
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2024-03-22 12:37:32 +0100

    legg til nonexit code

--- schema-registry/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/app/startup.kt
+++ schema-registry/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/app/startup.kt
@@ -1,10 +1,12 @@
 package no.nav.paw.arbeidssoekerregisteret.app
 
 import org.slf4j.LoggerFactory
+import kotlin.system.exitProcess
 
 
 fun main() {
     val logger = LoggerFactory.getLogger("app")
     logger.info("Starter")
     logger.info("Avsluttet")
+    exitProcess(1)
 }
