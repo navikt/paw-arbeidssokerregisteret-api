16c9def233d6e01d27548f38823fb9e11283288b
Nils Martin Sande <nils.martin.sande@nav.no>
2024-03-25 08:00:54 +0100

    La til ktor og isReady/isAlive

--- schema-registry/build.gradle.kts
+++ schema-registry/build.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.4.14")
     implementation("net.logstash.logback:logstash-logback-encoder:7.3")
     implementation("no.nav.common:log:3.2024.02.21_11.18-8f9b43befae1")
+
+    implementation("io.ktor:ktor-server-core:2.3.9")
+    implementation("io.ktor:ktor-server-netty:2.3.9")
+    implementation("io.confluent:kafka-schema-registry-client:7.6.0")
 }
 jib {
     from.image = "ghcr.io/navikt/baseimages/temurin:${jvmVersion.majorVersion}"
--- schema-registry/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/app/startup.kt
+++ schema-registry/src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/app/startup.kt
@@ -1,12 +1,59 @@
 package no.nav.paw.arbeidssoekerregisteret.app
 
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.*
 import org.slf4j.LoggerFactory
+import java.time.Duration
+import java.util.concurrent.CompletableFuture
 import kotlin.system.exitProcess
 
 
 fun main() {
     val logger = LoggerFactory.getLogger("app")
     logger.info("Starter")
-    logger.info("Avsluttet")
-    exitProcess(1)
+    val result = uploadSchema()
+        .exceptionally { exception ->
+            logger.error("Schema upload failed", exception)
+            HttpStatusCode.InternalServerError
+        }
+
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/isAlive") {
+                getCodeAndMsg(result).let { (code, msg) ->
+                    call.respond(code, msg)
+                }
+            }
+            get("/isReady") {
+                getCodeAndMsg(result).let { (code, msg) ->
+                    call.respond(code, msg)
+                }
+            }
+        }
+    }.start(wait = false)
+    result.join()
+    logger.info("Opplasting ferdig")
+    Thread.sleep(Duration.ofMinutes(5).toMillis())
+    exitProcess(0)
+}
+
+fun uploadSchema(): CompletableFuture<HttpStatusCode> =
+    CompletableFuture.supplyAsync {
+        Thread.sleep(1000)
+        HttpStatusCode.InternalServerError
+    }
+
+fun getCodeAndMsg(result: CompletableFuture<HttpStatusCode>): Pair<HttpStatusCode, String> = when {
+    !result.isDone -> Pair(HttpStatusCode.ServiceUnavailable, "not done yet")
+    result.isCancelled -> Pair(HttpStatusCode.InternalServerError, "Schema upload cancelled")
+    else -> Pair(
+        result.get(),
+        if (result.get() == HttpStatusCode.OK) "Schema upload successful" else "Schema upload failed"
+    )
 }
