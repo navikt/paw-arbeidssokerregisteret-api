6ede468847c7def4486547676c9d7866007acb34
Nils Martin Sande <nils.martin.sande@nav.no>
2025-09-12 07:16:04 +0200

    Oppdaterte avhengigheter

--- schema-registry/build.gradle.kts
+++ schema-registry/build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.davidmc24.gradle.plugin.avro.GenerateAvroProtocolTask
 
 plugins {
     kotlin("jvm")
-    id("com.google.cloud.tools.jib") version "3.4.4"
+    id("com.google.cloud.tools.jib") version "3.4.5"
     id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
     application
 }
@@ -16,13 +16,13 @@ val schema by configurations.creating {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.5.16")
-    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
-    implementation("no.nav.common:log:3.2024.05.23_05.46-2b29fa343e8e")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
+    implementation("no.nav.common:log:3.2025.06.23_14.50-3af3985d8555")
 
-    implementation("io.ktor:ktor-server-core:3.0.3")
-    implementation("io.ktor:ktor-server-netty:3.0.3")
-    implementation("io.confluent:kafka-schema-registry-client:7.8.0")
+    implementation("io.ktor:ktor-server-core:3.3.0")
+    implementation("io.ktor:ktor-server-netty:3.3.0")
+    implementation("io.confluent:kafka-schema-registry-client:8.0.0")
     implementation(project(":main-avro-schema"))
     implementation(project(":arena-avro-schema"))
     implementation(project(":bekreftelse-paavegneav-schema"))
